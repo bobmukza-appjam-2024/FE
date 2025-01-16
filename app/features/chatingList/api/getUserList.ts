06fec43017b15b4185b9e1a74a61e7387dc0146a
bae080311 <s24024@gsm.hs.kr>
2025-01-16 19:56:39 +0900

    Feat: get user list function

---
+++ app/features/chatingList/api/getUserList.ts
@@ -0,0 +1,13 @@
+import { User } from "@/app/entities/user";
+import axios from "axios";
+
+export default async function getUserList(): Promise<User[]> {
+  return await axios
+    .get(`${process.env.REACT_APP_API_URL}/chats`, {
+      headers: {},
+    })
+    .then((res) => {
+      return res.data;
+    });
+  throw new Error("Failed to fetch chat list");
+}
