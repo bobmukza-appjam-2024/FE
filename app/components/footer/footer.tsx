3ecc3b3c8fff6c0e0a0566e09599f22c4d1a7e28
bae080311 <s24024@gsm.hs.kr>
2025-01-17 00:49:31 +0900

    Feat: 경로로

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -3,23 +3,25 @@ import Schedule from "@/app/svgs/schedule";
 import Home from "@/app/svgs/home";
 import Chat from "@/app/svgs/chat";
 import MyPage from "@/app/svgs/myPage";
+import { useRouter } from "expo-router";
 
 export default function Footer() {
+  const r = useRouter();
   return (
     <S.Footer>
-      <S.Wrapper>
+      <S.Wrapper onPress={() => r.push("/pages/main")}>
         <Home />
         <S.Text>홈</S.Text>
       </S.Wrapper>
-      <S.Wrapper>
+      <S.Wrapper onPress={() => r.push("/pages/schedule")}>
         <Schedule />
         <S.Text>일정</S.Text>
       </S.Wrapper>
-      <S.Wrapper>
+      <S.Wrapper onPress={() => r.push("/pages/chatingList")}>
         <Chat />
         <S.Text>채팅</S.Text>
       </S.Wrapper>
-      <S.Wrapper>
+      <S.Wrapper onPress={() => r.push("/pages/profile")}>
         <MyPage />
         <S.Text>마이페이지</S.Text>
       </S.Wrapper>
