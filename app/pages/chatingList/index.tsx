f295b76348f9eb1597e11859f8cb109045aff073
bae080311 <s24024@gsm.hs.kr>
2025-01-16 19:57:05 +0900

    Feat: map chating user list

--- app/pages/chatingList/index.tsx
+++ app/pages/chatingList/index.tsx
@@ -1,5 +1,27 @@
+import { User } from "@/app/entities/user";
+import getUserList from "@/app/features/chatingList/api/getUserList";
+import UserCard from "@/app/features/chatingList/ui/userCard";
+import { useEffect, useState } from "react";
 import { View } from "react-native";
 
 export default function ChatingListPage() {
-  return <View></View>;
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const userList = await getUserList();
+        setUsers(userList);
+      } catch (error) {
+        console.error("Failed to fetch users:", error);
+      }
+    };
+    fetchUsers();
+  }, []);
+  return (
+    <View>
+      {users.map((v) => {
+        return <UserCard key={v.id} user={v} />;
+      })}
+    </View>
+  );
 }
