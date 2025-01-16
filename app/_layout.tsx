a511cf04fadb7be60570797aecb2a1378dd61a17
bae080311 <s24024@gsm.hs.kr>
2025-01-17 00:48:41 +0900

    Feat: Slot

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,12 @@ import "react-native-reanimated";
 import Header from "./components/header/header";
 import { View } from "react-native";
 import Footer from "./components/footer/footer";
+import { Slot } from "expo-router";
 
 export default function RootLayoutNav() {
   return (
     <View style={{ width: "100%", height: "100%" }}>
+      <Slot />
       <Header />
       <Footer />
     </View>
