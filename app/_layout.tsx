43eeceb820c417274c965098bed7c59cb8993799
bae080311 <s24024@gsm.hs.kr>
2025-01-17 00:20:46 +0900

    Feat: footer

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,13 @@
 import "react-native-reanimated";
 import Header from "./components/header/header";
 import { View } from "react-native";
+import Footer from "./components/footer/footer";
 
 export default function RootLayoutNav() {
   return (
-    <View>
+    <View style={{ width: "100%", height: "100%" }}>
       <Header />
+      <Footer />
     </View>
   );
 }
