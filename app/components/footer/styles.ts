c4ea122ec959e2c64eeaf199648ced51bec66336
bae080311 <s24024@gsm.hs.kr>
2025-01-17 00:20:28 +0900

    Feat: style

---
+++ app/components/footer/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components/native";
+
+export const Footer = styled.View`
+  flex-direction: row;
+  position: absolute;
+  bottom: 0;
+  justify-content: space-around;
+  width: 100%;
+  height: 80px;
+`;
+
+export const Text = styled.Text`
+  color: #9a9a9a;
+  font-family: Inter;
+  font-size: 12px;
+  font-weight: 400;
+`;
+
+export const Wrapper = styled.View`
+  align-items: center;
+  justify-content: center;
+  gap: 3px;
+`;
