f01053c47519744f13dc81d226ec4270f51e8db9
bae080311 <s24024@gsm.hs.kr>
2025-01-20 15:35:44 +0900

    Feat: title

--- app/pages/schedule/index.tsx
+++ app/pages/schedule/index.tsx
@@ -1,9 +1,12 @@
 import ShowToday from "@/app/features/schedule/ui/showToday";
 import * as S from "./styles";
+import ShowSchedule from "@/app/features/schedule/ui/showSchdule/showSchedule";
 
 export default function SchedulePage() {
   return (
     <S.Container>
+      <S.Title>오늘의 일정</S.Title>
+      <ShowSchedule />
       <ShowToday />
     </S.Container>
   );
--- app/pages/schedule/styles.ts
+++ app/pages/schedule/styles.ts
@@ -3,3 +3,10 @@ import styled from "styled-components/native";
 export const Container = styled.View`
   padding: 20px;
 `;
+
+export const Title = styled.Text`
+  color: #1d1d1d;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-weight: 600;
+`;
