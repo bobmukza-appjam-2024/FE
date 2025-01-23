8bc6b94673fad95dda2d0ba8e28152c20424e9d2
bae080311 <s24024@gsm.hs.kr>
2025-01-23 19:51:07 +0900

    Feat: input form

---
+++ app/components/input/input.tsx
@@ -0,0 +1,16 @@
+import { View } from "react-native";
+
+interface InputProps {
+  label: string;
+  setValue: (value: string) => void;
+}
+
+const Input = ({ label, setValue }: InputProps) => {
+  return (
+    <View>
+      <label htmlFor=""></label>
+      <input id={label} onChange={(e) => setValue(e.target.value)} />;
+    </View>
+  );
+};
+export default Input;
