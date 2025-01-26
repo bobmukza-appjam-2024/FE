1ec618c71f732cc0ae3c744f35cdbd83d33faf29
bae080311 <s24024@gsm.hs.kr>
2025-01-26 20:36:59 +0900

    Fix: button style

--- app/components/button/styles.ts
+++ app/components/button/styles.ts
@@ -6,12 +6,16 @@ export const Container = styled.TouchableOpacity`
   padding: 10px 20px;
   border-radius: 12px;
   border-color: #a1a1a1;
+  border-width: 1px;
   align-items: center;
   justify-content: center;
+  width: 100%;
 `;
 
 export const Text = styled.Text`
   color: #111;
   font-size: 16px;
   font-weight: 500;
+  width: 100%;
+  text-align: center;
 `;
