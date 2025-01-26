344fcc77c272452c2bff622685ae3c83eb4e918d
bae080311 <s24024@gsm.hs.kr>
2025-01-26 19:32:35 +0900

    Fix: props

--- app/components/button/button.tsx
+++ app/components/button/button.tsx
@@ -1,13 +1,15 @@
+import React from "react";
 import * as S from "./styles";
 
 interface ButtonProps {
   label: string;
-  func: () => void;
+  func: (...args: any[]) => void;
+  arg?: any[];
 }
 
-const Button = ({ label, func }: ButtonProps) => {
+const Button = ({ label, func, arg = [] }: ButtonProps) => {
   return (
-    <S.Container onPress={func}>
+    <S.Container onPress={() => func(...arg)}>
       <S.Text>{label}</S.Text>
     </S.Container>
   );
