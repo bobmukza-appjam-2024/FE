25154846eec28f4b44a350bb237d333399ef5c9f
bae080311 <s24024@gsm.hs.kr>
2025-02-02 14:59:12 +0900

    Feat: make user card clickable to navigate to chat page

--- app/features/chatingList/ui/userCard.tsx
+++ app/features/chatingList/ui/userCard.tsx
@@ -1,17 +1,27 @@
 import { User } from "@/app/entities/user";
+import { useRouter } from "expo-router";
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity } from "react-native";
 
 interface UserCardProps {
   user: User;
 }
 
 const UserCard = ({ user }: UserCardProps) => {
+  const router = useRouter();
+
   return (
-    <View>
+    <TouchableOpacity
+      onPress={() => {
+        router.push({
+          pathname: "/pages/chating/[id]/index",
+          params: { id: user.id },
+        });
+      }}
+    >
       <Image source={require("../../../svgs/profile")} />
       <Text>{user.nickname}</Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
