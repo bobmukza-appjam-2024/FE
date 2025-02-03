4787bef399ce2532dc7c1e907615007ea9942ce1
bae080311 <s24024@gsm.hs.kr>
2025-02-03 19:56:00 +0900

    Feat: implement login page layout with styled components and input fields

--- app/pages/login/index.tsx
+++ app/pages/login/index.tsx
@@ -1,9 +1,14 @@
-import { Text, View } from "react-native";
+import ShowInputs from "@/app/features/login/ui/showInputs";
+import * as S from "./styles";
 
 export default function LoginPage() {
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <S.Container>
+      <ShowInputs />
+      <S.회원아님Wrapper>
+        <S.회원가입Text1>회원이 아니신가요?</S.회원가입Text1>
+        <S.회원가입Text2>회원가입</S.회원가입Text2>
+      </S.회원아님Wrapper>
+    </S.Container>
   );
 }
---
+++ app/pages/login/styles.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  padding: 20px;
+  gap: 5px;
+  background-color: #fafafa;
+`;
+
+export const 회원아님Wrapper = styled.View`
+  flex-direction: row;
+  gap: 10px;
+  justify-content: center;
+`;
+
+export const 회원가입Text1 = styled.Text`
+  color: #8f9094;
+  text-align: center;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 22px;
+`;
+export const 회원가입Text2 = styled.Text`
+  color: #3269f6;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 22px;
+`;
