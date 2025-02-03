63461cfb253f91352e06100923df50d29f9b6d78
bae080311 <s24024@gsm.hs.kr>
2025-02-03 20:09:13 +0900

    Feat: add ShowInputs component for login functionality with email and password fields

---
+++ app/features/login/ui/showInputs.tsx
@@ -0,0 +1,29 @@
+import Button from "@/app/components/button/button";
+import Input from "@/app/components/input/input";
+import { useState } from "react";
+import { View } from "react-native";
+import { submitLogin } from "../api/submitLogin";
+
+const ShowInputs = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <View style={{ gap: 420 }}>
+      <View style={{ gap: 20 }}>
+        <Input
+          placeholder="아이디를 입력해주세요"
+          setValue={setEmail}
+          label="아이디"
+        />
+        <Input
+          placeholder="비밀번호를 입력해주세요"
+          setValue={setPassword}
+          label="비밀번호"
+        />
+      </View>
+      <Button label="로그인" func={() => submitLogin(email, password)} />
+    </View>
+  );
+};
+
+export default ShowInputs;
