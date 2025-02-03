1ab6ad031d99eaa04b6224db56434590ed44aa14
bae080311 <s24024@gsm.hs.kr>
2025-02-03 19:46:40 +0900

    Feat: 하단 바 숨기기기

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,19 +2,18 @@ import "react-native-reanimated";
 import Header from "./components/header/header";
 import { View } from "react-native";
 import Footer from "./components/footer/footer";
-import { Slot, Redirect, useRootNavigationState } from "expo-router";
+import { Slot, useSegments } from "expo-router";
 
 export default function RootLayoutNav() {
-  const navigationState = useRootNavigationState();
-
-  if (!navigationState?.key) return null;
-
+  const segments = useSegments() as string[];
+  const hideBottomBar =
+    segments.includes("login") || segments.includes("signup");
   return (
     <View style={{ width: "100%", height: "100%", backgroundColor: "#f5f5f5" }}>
       <Header />
-      <Redirect href="/pages/login" />
+
       <Slot />
-      <Footer />
+      {!hideBottomBar && <Footer />}
     </View>
   );
 }
