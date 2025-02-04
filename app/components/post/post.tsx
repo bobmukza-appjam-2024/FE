4228bd5253306cad07bd6e9242749d11ad7ebfad
bae080311 <s24024@gsm.hs.kr>
2025-02-04 21:38:04 +0900

    Feat: add Post component for displaying menu and category information

---
+++ app/components/post/post.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+const Post = () => {
+  return (
+    <View>
+      <Text>날짜짜</Text>
+      <View>
+        <Text>메뉴</Text>
+        <Text>sort</Text>
+      </View>
+      <Text>category</Text>
+    </View>
+  );
+};
+
+export default Post;
