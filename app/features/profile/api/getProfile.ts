670150548b1e5defd1f9f80e1a85c9669a281cff
bae080311 <s24024@gsm.hs.kr>
2025-02-06 02:01:08 +0900

    Feat: update getProfile function to use constant API_URL and improve token handling

--- app/features/profile/api/getProfile.ts
+++ app/features/profile/api/getProfile.ts
@@ -1,13 +1,13 @@
 import { Profile } from "@/app/entities/profile";
+import { API_URL } from "@/constants/url";
 import axios from "axios";
+import { getToken } from "../../login/api/getToken";
 
-export async function getProfile(
-  id: string | undefined
-): Promise<Profile | undefined> {
+export async function getProfile(): Promise<Profile | undefined> {
   try {
-    const response = await axios.get(`${process.env.URL}/profile`, {
+    const response = await axios.get(`${API_URL}/profile`, {
       headers: {
-        // 토큰
+        Authorization: await getToken(),
       },
     });
     return response.data;
