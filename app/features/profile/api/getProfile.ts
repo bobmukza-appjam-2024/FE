85d15b484e4202add446853ab5676b68c9809250
bae080311 <s24024@gsm.hs.kr>
2025-02-05 19:26:19 +0900

    Fix: get profile

--- app/features/profile/api/getProfile.ts
+++ app/features/profile/api/getProfile.ts
@@ -1,6 +1,9 @@
+import { Profile } from "@/app/entities/profile";
 import axios from "axios";
 
-export async function getProfile() {
+export async function getProfile(
+  id: string | undefined
+): Promise<Profile | undefined> {
   try {
     const response = await axios.get(`${process.env.URL}/profile`, {
       headers: {
