930b2c799d641f4251d3a8e2905421912c7df807
bae080311 <s24024@gsm.hs.kr>
2025-02-05 22:38:45 +0900

    Feat: add nickname input field to ReviseProfile component

--- app/features/editProfile/ui/reviseProfile.tsx
+++ app/features/editProfile/ui/reviseProfile.tsx
@@ -6,23 +6,29 @@ import reviseIntroduce from "../api/reviseIntroduce";
 import Input from "@/app/components/input/input";
 
 const ReviseProfile = () => {
+  const [nickname, setNickname] = useState<string>("");
   const [introduction, setIntroduction] = useState<string>("");
   const [favorite, setFavorite] = useState<string>("");
   return (
     <View>
-      <Textarea
-        label="My 소개글"
-        placeholder="소개글을 입력해주세요"
-        setValue={setIntroduction}
+      <Input
+        label="이름"
+        placeholder="이름을 입력해주세요"
+        setValue={setNickname}
       />
       <Input
         label="취향"
         placeholder="취향을 입력해주세요"
         setValue={setFavorite}
       />
+      <Textarea
+        label="My 소개글"
+        placeholder="소개글을 입력해주세요"
+        setValue={setIntroduction}
+      />
       <Button
         func={reviseIntroduce}
-        arg={[introduction, favorite]}
+        arg={[introduction, favorite, nickname]}
         label="수정하기"
       />
     </View>
