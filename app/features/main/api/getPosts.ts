c4ca5822853550664bebe13c9a98902e4a0b4494
bae080311 <s24024@gsm.hs.kr>
2025-02-10 19:25:49 +0900

    Feat: update getPosts API to return an array of PostType and handle errors gracefully

--- app/features/main/api/getPosts.ts
+++ app/features/main/api/getPosts.ts
@@ -1,14 +1,17 @@
 import { API_URL } from "@/constants/url";
 import axios from "axios";
 import { getToken } from "../../login/api/getToken";
-import { Post } from "@/app/entities/post";
+import { PostType } from "@/app/entities/post";
 
 interface GetPostsParams {
   page: number;
   mealTime?: string;
 }
 
-export const getPosts = ({ page, mealTime }: GetPostsParams): Post | void => {
+export const getPosts = ({
+  page,
+  mealTime,
+}: GetPostsParams): PostType[] | void => {
   axios
     .get(`${API_URL}/posts?page=${page}&size=${3}&mealTime=${mealTime}`, {
       headers: {
@@ -20,5 +23,6 @@ export const getPosts = ({ page, mealTime }: GetPostsParams): Post | void => {
     })
     .catch((err) => {
       console.error(err);
+      return null;
     });
 };
