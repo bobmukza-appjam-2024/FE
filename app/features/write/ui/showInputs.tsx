e0cf90153121bccd394c174f1aa960a2f857b7ab
bae080311 <s24024@gsm.hs.kr>
2025-02-10 20:01:36 +0900

    Feat: refactor ShowInputs component to use a single form state object and improve input handling

--- app/features/write/ui/showInputs.tsx
+++ app/features/write/ui/showInputs.tsx
@@ -6,42 +6,58 @@ import { View } from "react-native";
 import { submitPost } from "../api/submitPost";
 
 const ShowInputs = () => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [mealTime, setMealTime] = useState("");
-  const [location, setLocation] = useState("");
-  const [menuName, setMenuName] = useState("");
+  const [form, setForm] = useState({
+    title: "",
+    content: "",
+    mealTime: "",
+    location: "",
+    menuName: "",
+  });
+
+  const handleChange = (key: keyof typeof form, value: string) => {
+    setForm((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+
   return (
-    <View>
+    <View style={{ gap: 20 }}>
       <Input
         label="제목"
-        setValue={setTitle}
+        setValue={(value) => handleChange("title", value)}
         placeholder="제목을 입력해주세요"
       />
       <Input
         label="메뉴명"
-        setValue={setMenuName}
+        setValue={(value) => handleChange("menuName", value)}
         placeholder="메뉴명을 입력해주세요"
       />
       <Input
         label="식사 시간"
-        setValue={setMealTime}
+        setValue={(value) => handleChange("mealTime", value)}
         placeholder="식사 시간을 입력해주세요"
       />
       <Input
         label="장소"
-        setValue={setLocation}
+        setValue={(value) => handleChange("location", value)}
         placeholder="장소를 입력해주세요"
       />
       <Textarea
         label="추가 사항"
         placeholder="추가 사항을 입력해주세요"
-        setValue={setContent}
+        setValue={(value) => handleChange("content", value)}
       />
       <Button
         func={submitPost}
         label="생성하기"
-        arg={[title, content, mealTime, location, menuName]}
+        arg={[
+          form.title,
+          form.content,
+          form.mealTime,
+          form.location,
+          form.menuName,
+        ]}
       />
     </View>
   );
