fff873c5606a4c7928cb434c28950b415197726e
bae080311 <s24024@gsm.hs.kr>
2025-02-10 20:28:52 +0900

    Feat: implement Main component with ShowPosts display and navigation link to WritePage

--- app/pages/main/index.tsx
+++ app/pages/main/index.tsx
@@ -1,5 +1,30 @@
-import { View } from "react-native";
+import ShowPosts from "@/app/features/main/ui/showPosts";
+import { Container } from "./styles";
+import { Text } from "react-native";
+import Plus from "@/app/svgs/plus";
+import { Link } from "expo-router";
 
 export default function Main() {
-  return <View></View>;
+  return (
+    <Container>
+      <ShowPosts />
+      <Link
+        style={{
+          width: "100%",
+          borderWidth: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          padding: 10,
+          borderRadius: 12,
+          borderColor: "#a1a1a1",
+          textAlign: "center",
+          gap: 10,
+        }}
+        href={"/pages/write"}
+      >
+        <Text style={{ fontSize: 14 }}>밥 약속 추가하기</Text>
+        <Plus />
+      </Link>
+    </Container>
+  );
 }
