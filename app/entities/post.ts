75c8da2db553a98c6523ed61dad36bbb919fcd65
bae080311 <s24024@gsm.hs.kr>
2025-02-10 19:35:18 +0900

    Fix: post type

--- app/entities/post.ts
+++ app/entities/post.ts
@@ -1,18 +1,12 @@
-export interface Post {
-  posts: [
-    {
-      id: number;
-      title: string;
-      mealTime: string;
-      location: string;
-      menuName: string;
-      author: {
-        nickname: string;
-        age: number;
-      };
-      createdAt: string;
-    }
-  ];
-  totalPages: number;
-  currentPage: number;
+export interface PostType {
+  id: number;
+  title: string;
+  mealTime: string;
+  location: string;
+  menuName: string;
+  author: {
+    nickname: string;
+    age: number;
+  };
+  createdAt: string;
 }
